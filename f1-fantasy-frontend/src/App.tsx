5e8a753174bb27ffae6bd640e0976748ac25d630
Mahir Prcanović <mahirprcanovic7@gmail.com>
2022-06-13 00:20:30 +0200

    feat: axios implemented

--- f1-fantasy-frontend/src/App.tsx
+++ f1-fantasy-frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import "./App.css";
-
+import axios from "axios";
 function App() {
+  axios.get("https://jsonplaceholder.typicode.com/posts/1").then((response) => {
+    // handle success
+    console.log(response.data);
+  });
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
