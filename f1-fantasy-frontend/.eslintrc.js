0dc3e87b995f859f8c44ddd162c88069e4fb5cac
Mirzad <52415551+MirzadVa@users.noreply.github.com>
2022-06-13 13:13:13 +0200

    feat: added linter

---
+++ f1-fantasy-frontend/.eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      jest: true,
+    },
+    extends: [
+      'react-app',
+      'react-app/jest',
+      'airbnb',
+      'airbnb-typescript',
+      'plugin:prettier/recommended',
+      'plugin:react/jsx-runtime',
+      'prettier',
+    ],
+    plugins: ['prettier', 'jsx-a11y', '@typescript-eslint'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      project: './tsconfig.json',
+    },
+    rules: {
+      'import/extensions': [1, 'never'],
+      'import/prefer-default-export': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
+      'consistent-return': 'off',
+      'react/prop-types': 'off',
+      'react/require-default-props': 'off',
+      'react/function-component-definition': 'off',
+      'jsx-a11y/label-has-associated-control': [
+        2,
+        {
+          labelComponents: ['label'],
+          labelAttributes: ['label'],
+          controlComponents: ['Input'],
+          depth: 3,
+        },
+      ],
+      'prefer-destructuring': 'off',
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          paths: ['src'],
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
+    },
+  };
--- f1-fantasy-frontend/src/App.tsx
+++ f1-fantasy-frontend/src/App.tsx
@@ -1,11 +1,14 @@
-import "./App.css";
-import axios from "axios";
+import './App.css';
+import axios from 'axios';
+
+const test = { ime: 'a', prezime: 'b', ovo: 'c' };
+
 function App() {
-  axios.get("https://jsonplaceholder.typicode.com/posts/1").then((response) => {
+  axios.get('https://jsonplaceholder.typicode.com/posts/1').then((response) => {
     // handle success
     console.log(response.data);
   });
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <div>aa</div>;
 }
 
 export default App;
