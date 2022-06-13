0a39f8e27944d26703068e48052f5816b4387ae6
Mahir Prcanović <mahirprcanovic7@gmail.com>
2022-06-13 14:16:44 +0200

    fix: prettier problem fixed

--- f1-fantasy-frontend/src/App.tsx
+++ f1-fantasy-frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css';
-import axios from 'axios';
 
 function App() {
-  axios.get('https://jsonplaceholder.typicode.com/posts/1').then((response) => {
-    // handle success
-    console.log(response.data);
-  });
-  return <div>aa</div>;
+  return (
+    <section>
+      <div>TESt</div>
+    </section>
+  );
 }
 
 export default App;
